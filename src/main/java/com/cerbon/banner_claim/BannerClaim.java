cfd6b10229d8b300231e73af9727a9955c518e28
Felipe <felipecerboncinix@gmail.com>
2024-02-06 11:35:49 -0300

    Initial commit

---
+++ src/main/java/com/cerbon/banner_claim/BannerClaim.java
@@ -0,0 +1,13 @@
+package com.cerbon.banner_claim;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(BannerClaim.MOD_ID)
+public class BannerClaim {
+    public static final String MOD_ID = "banner_claim";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BannerClaim() {}
+}
