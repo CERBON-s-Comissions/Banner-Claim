1254554b09afa4a8f93777026e96de390d884774
Felipe <felipecerboncinix@gmail.com>
2024-02-12 01:31:43 -0300

    Add range to check based on the netherite range

--- src/main/java/com/cerbon/banner_claim/util/BCUtils.java
+++ src/main/java/com/cerbon/banner_claim/util/BCUtils.java
@@ -1,6 +1,7 @@
 package com.cerbon.banner_claim.util;
 
 import com.cerbon.banner_claim.block.BCBlocks;
+import com.cerbon.banner_claim.block.custom.BannerTier;
 import com.cerbon.banner_claim.block.custom.entity.BannerClaimBlockEntity;
 import com.cerbon.banner_claim.capability.BCCapabilities;
 import net.minecraft.core.BlockPos;
@@ -17,8 +18,10 @@ public class BCUtils {
 
     public static void ifBannerClaimContainsChunkDo(ChunkPos chunkPos, ServerLevel serverLevel, BiConsumer<BlockPos, BannerClaimBlockEntity> biConsumer) {
         BCCapabilities.getChunkBlockCache(serverLevel).ifPresent(chunkCache -> {
-            for (int x = chunkPos.x - 8; x <= chunkPos.x + 8; x++)
-                for (int z = chunkPos.z - 8; z <= chunkPos.z + 8; z++) {
+            int rangeToCheck = Math.round((float) BannerClaimBlockEntity.getBannerTierRange(BannerTier.NETHERITE) / 16);
+
+            for (int x = chunkPos.x - rangeToCheck; x <= chunkPos.x + rangeToCheck; x++)
+                for (int z = chunkPos.z - rangeToCheck; z <= chunkPos.z + rangeToCheck; z++) {
                     List<BlockPos> blocks = chunkCache.getBlocksFromChunk(new ChunkPos(x, z), BCBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).toArray(Block[]::new));
                     if (blocks == null) continue;
 
