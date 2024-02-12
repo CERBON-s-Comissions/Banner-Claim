ae628eeeabd20cc31067b9544cec283203dafedb
Felipe <felipecerboncinix@gmail.com>
2024-02-11 22:17:54 -0300

    Prevent players that are not the owner of the banner to place blocks

--- src/main/java/com/cerbon/banner_claim/event/ForgeEvents.java
+++ src/main/java/com/cerbon/banner_claim/event/ForgeEvents.java
@@ -8,12 +8,14 @@ import com.cerbon.banner_claim.capability.BCCapabilities;
 import com.cerbon.banner_claim.capability.custom.ChunkBlockCacheProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.EntityMobGriefingEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,29 +33,38 @@ public class ForgeEvents {
         event.addCapability(new ResourceLocation(BannerClaim.MOD_ID, "chunk_block_cache_capability"), new ChunkBlockCacheProvider());
     }
 
-//    @SubscribeEvent
-//    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-//        if (event.side == LogicalSide.CLIENT) return;
-//
-//        ServerPlayer player = (ServerPlayer) event.player;
-//        IEntityMixin playerMixin = ((IEntityMixin) player);
-//        BannerClaimBlockEntity bannerClaimBlockEntity = playerMixin.getBannerClaimBlockEntity();
-//
-//        if (bannerClaimBlockEntity == null || player.isCreative() || player.isSpectator()) return;
-//
-//        boolean isInBannerRange = bannerClaimBlockEntity.playersInBox.contains(player);
-//        boolean isOwner = player == bannerClaimBlockEntity.getOwner();
-//
-//        if (isInBannerRange && !isOwner) {
-//            player.getAbilities().mayBuild = false;
-//            player.connection.send(new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
-//
-//        } else if (!player.getAbilities().mayBuild && playerMixin.getBannerClaimBlockEntity() != null) {
-//            event.player.getAbilities().mayBuild = true;
-//            player.connection.send(new ClientboundPlayerAbilitiesPacket(player.getAbilities()));
-//            playerMixin.setBannerClaimBlockEntity(null);
-//        }
-//    }
+    @SubscribeEvent
+    public static void onPlaceBlock(BlockEvent.EntityPlaceEvent event) {
+        if (event.getLevel().isClientSide() || event.getEntity() == null) return;
+
+        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
+            BCCapabilities.getChunkBlockCache(serverPlayer.level()).ifPresent(capability -> {
+                ChunkPos chunkPos = new ChunkPos(event.getPos());
+
+                for (int x = chunkPos.x - 8; x <= chunkPos.x + 8; x++)
+                    for (int z = chunkPos.z - 8; z <= chunkPos.z + 8; z++) {
+                        List<BlockPos> blocks = capability.getBlocksFromChunk(new ChunkPos(x, z), BCBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).toArray(Block[]::new));
+
+                        for (BlockPos blockPos : blocks) {
+                            BlockEntity blockEntity = serverPlayer.level().getBlockEntity(blockPos);
+
+                            if (blockEntity instanceof BannerClaimBlockEntity bannerClaimBlockEntity) {
+                                BannerTier tier = bannerClaimBlockEntity.getBannerTier();
+                                int bannerTierRange  = BannerClaimBlockEntity.getBannerTierRange(tier);
+
+                                boolean isWithinBannerRange = Math.abs(blockPos.getX() - event.getPos().getX()) <= bannerTierRange && Math.abs(blockPos.getY() - 10) <= event.getPos().getY() && Math.abs(blockPos.getZ() - event.getPos().getZ()) <= bannerTierRange;
+                                boolean isOwner = serverPlayer == bannerClaimBlockEntity.getOwner();
+
+                                if (isWithinBannerRange && !isOwner) {
+                                    event.setCanceled(true);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+            });
+        }
+    }
 
     @SubscribeEvent
     public static void onExplosion(ExplosionEvent.Detonate event) {
