f120344147c3b9234d01aca2b4234845dc6240da
Felipe <felipecerboncinix@gmail.com>
2024-02-11 22:36:08 -0300

    Remove entity mixin

--- src/main/java/com/cerbon/banner_claim/util/mixin/IEntityMixin.java
+++
@@ -1,9 +0,0 @@
-package com.cerbon.banner_claim.util.mixin;
-
-import com.cerbon.banner_claim.block.custom.entity.BannerClaimBlockEntity;
-
-public interface IEntityMixin {
-    BannerClaimBlockEntity getBannerClaimBlockEntity();
-
-    void setBannerClaimBlockEntity(BannerClaimBlockEntity bannerClaimBlockEntity);
-}
